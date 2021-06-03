f6a03b25cd806c71f83fefed5bc204fced3d89f1
habrasek <65578198+habrasek@users.noreply.github.com>
2021-06-03 19:23:51 -0400

    parts 4, 5, and 6 done

--- EmployeeSQL/code.sql
+++ EmployeeSQL/code.sql
@@ -27,20 +27,30 @@ ON dm.emp_no = e.emp_no
 --4. List the department of each employee with the following information: 
 --employee number, last name, first name, and department name.
 
-SELECT e.emp_no, e.last_name, e.first_name, de.dept_no
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
 INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no
 ;
 
-SELECT d.dept_name, de.dept_no
-FROM departments AS d
-INNER JOIN dept_emp AS de ON d.dept_no = de.dept_no;
+
+--5. List first name, last name, and sex for employees whose first name is 
+--"Hercules" and last names begin with "B."
+
+SELECT first_name, last_name, sex --, SUBSTRING(last_name,1,1)
+FROM employees
+WHERE first_name = 'Hercules' AND SUBSTRING(last_name,1,1) = 'B'
+;
+
+--6. List all employees in the Sales department, including their employee number, 
+--last name, first name, and department name.
 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
+INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
 ;
-
-
 
 
 
